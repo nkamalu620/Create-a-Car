eeb7168212e54f3f0d9c3f4a1730407486c5f7ce
nkamalu620 <nkamalu96@gmail.com>
2024-12-12 19:04:29 -0500

    updated code for motorbike.ts and truck.ts

--- Develop/src/classes/Truck.ts
+++ Develop/src/classes/Truck.ts
@@ -6,16 +6,50 @@ import Wheel from './Wheel.js';
 import AbleToTow from '../interfaces/AbleToTow.js';
 
 // TODO: The Truck class should extend the Vehicle class and should implement the AbleToTow interface
-class Truck {
+class Truck extends Vehicle{
   // TODO: Declare properties of the Truck class
   // TODO: The properties should include vin, color, make, model, year, weight, top speed, wheels, and towing capacity
   // TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[]), towingCapacity (number)
+  vin: string;
+  color: string;
+  make: string;
+  model: string;
+  year: number;
+  weight: number;
+  topSpeed: number;
+  wheels: Wheel[];
+  towingCpacity: number;
 
   // TODO: Create a constructor that accepts the properties of the Truck class
-    // TODO: The constructor should call the constructor of the parent class, Vehicle
-    // TODO: The constructor should initialize the properties of the Truck class
-    // TODO: The constructor should check if the wheels array has 4 elements and create 4 new default Wheel objects if it does not
-
+  onstructor(
+    vin: string,
+    color: string,
+    make: string,
+    model: string,
+    year: number,
+    weight: number,
+    topSpeed: number,
+    wheels: Wheel[],
+    towingCapacity: number
+  ) {
+  // TODO: The constructor should call the constructor of the parent class, Vehicle
+  super();
+  // TODO: The constructor should initialize the properties of the Truck class
+  this.vin = vin;
+  this.color = color;
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.weight = weight;
+  this.topSpeed = topSpeed;
+  this.towingCpacity = towingCapacity;
+  // TODO: The constructor should check if the wheels array has 4 elements and create 4 new default Wheel objects if it does not
+  if (wheels.length !== 4) {
+    this.wheels = [new Wheel(), new Wheel(), new Wheel(), new Wheel()];
+  } else {
+    this.wheels = wheels;
+  }
+}
   // TODO: Implement the tow method from the AbleToTow interface
   tow(vehicle: Truck | Motorbike | Car): void {
     // TODO: Get the make an model of the vehicle if it exists
@@ -28,7 +62,21 @@ class Truck {
     // TODO: The method should call the printDetails method of the parent class
     // TODO: The method should log the details of the Truck
     // TODO: The details should include the VIN, make, model, year, weight, top speed, color, towing capacity, and wheels
-}
+    override printDetails(): void {
+    
+      super.printDetails();
+  
+      console.log(`VIN: ${this.vin}`);
+      console.log(`Color: ${this.color}`);
+      console.log(`Make: ${this.make}`);
+      console.log(`Model: ${this.model}`);
+      console.log(`Year: ${this.year}`);
+      console.log(`Weight: ${this.weight} lbs`);
+      console.log(`Top Speed: ${this.topSpeed} mph`);
+      console.log(`Wheels: ${this.wheels}`);
+      console.log(`Towing Cpacity: ${this.towingCapacity}`);
+    }
+  }
 
 // Export the Truck class as the default export
 export default Truck;
